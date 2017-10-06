88046ddfe43966e9021831aabab65ff2bd2258c4
Jimmy Zhang <codgician@users.noreply.github.com>
2017-10-06 21:40:46 +0800

    HDUOJ - 1095

---
+++ HDUOJ/1095/water.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int a, b;
+    while (cin >> a >> b)
+        cout << a + b << endl << endl;
+    return 0;
+}
