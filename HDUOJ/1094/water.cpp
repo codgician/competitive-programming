c865f8b24a6ddb50e310af2a12e36f1c617dae6a
Jimmy Zhang <codgician@users.noreply.github.com>
2017-10-06 21:38:57 +0800

    HDUOJ - 1094

---
+++ HDUOJ/1094/water.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    int num;
+    while (cin >> num)
+    {
+        int sum = 0;
+        for (int i = 0; i < num; i++)
+        {
+            int tmp;
+            cin >> tmp;
+            sum += tmp;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
