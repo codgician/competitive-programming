6d84e383f3cd0ae5cc8f935e797ece14ab850721
Jimmy Zhang <codgician@users.noreply.github.com>
2017-10-28 21:54:05 +0800

    HDUOJ - 1003: Add comments

--- HDUOJ/1003/greedy.cpp
+++ HDUOJ/1003/greedy.cpp
@@ -1,3 +1,10 @@
+// This is actually an optimized version of dp.cpp
+// We can easily figure out its state transition equation:
+// dp[i] = max(dp[i - 1] + arr[i], arr[i])
+// It means that the only thing that matters is whether dp[i - 1] is positive or not!
+// So if the previous sub-sequence has a sum smaller than 0, why need it?
+// The sum must be bigger if we abandon them and start a new sub-sequence!
+
 #include <iostream>
 #include <cstdio>
 #include <climits>
@@ -19,6 +26,8 @@ int main()
         {
             int tmp;
             cin >> tmp;
+
+            // if dp[i - 1] < 0
             if (sum < 0)
             {
                 tStartPt = t;
