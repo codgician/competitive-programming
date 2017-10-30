156931531e31269be115950353d00e8556dab1ed
Jimmy Zhang <codgician@users.noreply.github.com>
2017-10-30 09:00:02 +0800

    HDUOJ - 2084

---
+++ HDUOJ/2084/dp.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#define SIZE 101
+using namespace std;
+
+int arr[SIZE][SIZE], dp[SIZE];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int caseNum;
+    cin >> caseNum;
+    for (int t = 0; t < caseNum; t++)
+    {
+        int height;
+        cin >> height;
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+
+        for (int j = 0; j < height; j++)
+            dp[j] = arr[height - 1][j];
+
+        for (int i = height - 2; i >= 0; i--)
+        {
+            for (int j = 0; j <= i; j++)
+                dp[j] = max(dp[j], dp[j + 1]) + arr[i][j];
+        }
+
+        cout << dp[0] << endl;
+    }
+    return 0;
+}
--- POJ/1163/dp_space_saving.cpp
+++ POJ/1163/dp_space_saving.cpp
@@ -4,38 +4,25 @@
 #define SIZE 101
 using namespace std;
 
-int arr[SIZE][SIZE], dp[2][SIZE];
+int arr[SIZE][SIZE], dp[SIZE];
 
 int main()
 {
     ios::sync_with_stdio(false);
     int height;
     cin >> height;
+
     for (int i = 0; i < height; i++)
-    {
         for (int j = 0; j <= i; j++)
-        {
-            int tmp;
-            cin >> tmp;
-            arr[i][j] = tmp;
-            if (i == height - 1)
-                dp[1][j] = tmp;
-        }
-    }
+            cin >> arr[i][j];
+
+	for (int j = 0; j < height; j++)
+		dp[j] = arr[height - 1][j];
 
     for (int i = height - 2; i >= 0; i--)
-    {
         for (int j = 0; j <= i; j++)
-        {
-            dp[0][j] = max(dp[1][j], dp[1][j + 1]) + arr[i][j];
-        }
-        for (int j = 0; j <= i; j++)
-        {
-            dp[1][j] = dp[0][j];
-        }
-    }
+            dp[j] = max(dp[j], dp[j + 1]) + arr[i][j];
 
-    cout << dp[0][0] << endl;
-
+    cout << dp[0] << endl;
     return 0;
 }
