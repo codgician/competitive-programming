8556181f6a1ac5a59b34cac6f75c9d46f2327cd5
codgician <codgician@users.noreply.github.com>
2017-12-03 11:45:31 +0800

    HDUOJ - 2199: Binary Search

---
+++ HDUOJ/2199/binary_search.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <iomanip>
+#include <climits>
+using namespace std;
+
+double y;
+
+double calc(double x)
+{
+    return 8 * pow(x, 4) + 7 * pow(x, 3) + 2 * pow(x, 2) + 3 * x + 6;
+}
+
+double work(double left, double right)
+{
+    double mid = (left + right) / 2;
+
+    if (fabs(calc(mid) - y) < 1e-5)
+        return mid;
+
+    if (calc(mid) > y)
+        return work(left, mid - 1);
+    else
+        return work(mid + 1, right);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int caseNum;
+    cin >> caseNum;
+    for (int t = 0; t < caseNum; t++)
+    {
+        cin >> y;
+        if (calc(0) > y || calc(100) < y)
+            cout << "No solution!" << endl;
+        else
+            cout << fixed << setprecision(4) << work(1, 100) << endl;
+
+    }
+    return 0;
+}
