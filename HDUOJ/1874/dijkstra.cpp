119034c33c94fb3c1057f8171694b3eb115512ec
Jimmy Zhang <codgician@users.noreply.github.com>
2018-02-02 22:24:31 +0800

    HDUOJ - 1874: Dijkstra

---
+++ HDUOJ/1874/dijkstra.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <climits>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#define SIZE 201
+#define INF 100000001
+using namespace std;
+
+int arr[SIZE][SIZE];
+int dis[SIZE];
+bool isShortest[SIZE];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int townNum, roadNum;
+    while (cin >> townNum >> roadNum)
+    {
+        for (int i = 0; i < townNum; i++)
+        {
+            for (int j = 0; j < townNum; j++)
+            {
+                if (i == j)
+                    arr[i][j] = 0;
+                else
+                    arr[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < roadNum; i++)
+        {
+            int from, to, len;
+            cin >> from >> to >> len;
+            if (len < arr[from][to])
+            {
+                arr[from][to] = len;
+                arr[to][from] = len;
+            }
+        }
+
+        int startPt, endPt;
+        cin >> startPt >> endPt;
+
+        memset(isShortest, false, sizeof(isShortest));
+        isShortest[startPt] = true;
+        for (int i = 0; i < townNum; i++)
+        {
+            dis[i] = arr[startPt][i];
+        }
+
+        for (int t = 0; t < townNum - 1; t++)
+        {
+            int minLen = INF, minPt = -1;
+            for (int i = 0; i < townNum; i++)
+            {
+                if (!isShortest[i] && dis[i] < minLen)
+                {
+                    minLen = dis[i];
+                    minPt = i;
+                }
+            }
+
+            if (minPt == -1)
+                break;
+
+            isShortest[minPt] = true;
+            for (int i = 0; i < townNum; i++)
+            {
+                if (arr[minPt][i] != INF)
+                {
+                    dis[i] = min(dis[i], dis[minPt] + arr[minPt][i]);
+                }
+            }
+        }
+
+        if (dis[endPt] == INF)
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            cout << dis[endPt] << endl;
+        }
+    }
+    return 0;
+}
