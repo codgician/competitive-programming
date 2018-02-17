cc5b9efb5adf580d3e7f695012ef6a73ce0c2658
Jimmy Zhang <codgician@users.noreply.github.com>
2018-02-17 21:01:18 +0800

    SPOJ - 1: Implementation

---
+++ SPOJ/1/implementation.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <climits>
+#include <vector>
+#include <set>
+#include <queue>
+#include <map>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int num;
+    while (cin >> num)
+    {
+        if (num == 42)
+            break;
+        cout << num << endl;
+    }
+    return 0;
+}
