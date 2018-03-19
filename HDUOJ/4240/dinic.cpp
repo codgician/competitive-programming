732f663c663635811a512ab939d7592c5d54a671
Jimmy Zhang <codgician@users.noreply.github.com>
2018-03-19 14:51:37 +0800

    HDUOJ - 4240: Dinic

---
+++ HDUOJ/4240/dinic.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <climits>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#define VERTEX_SIZE 1001
+#define EDGE_SIZE 2002000
+using namespace std;
+
+typedef struct _Edge
+{
+    int to;
+    int capacity;
+    int next;
+} Edge;
+
+Edge arr[EDGE_SIZE];
+int head[VERTEX_SIZE], arrPt;
+int depth[VERTEX_SIZE], lastVisitedEdge[VERTEX_SIZE];
+int vertexNum, maxCapacity;
+
+void addEdge(int from, int to, int capacity)
+{
+    arr[arrPt] = {to, capacity, head[from]};
+    head[from] = arrPt++;
+    arr[arrPt] = {from, 0, head[to]};
+    head[to] = arrPt++;
+}
+
+bool updateDepth(int startPt, int endPt)
+{
+    memset(depth, -1, sizeof(depth));
+    queue<int> q;
+
+    q.push(startPt);
+    depth[startPt] = 0;
+
+    while (!q.empty())
+    {
+        int cntPt = q.front();
+        q.pop();
+
+        int edgePt = head[cntPt];
+        while (edgePt != -1)
+        {
+            if (depth[arr[edgePt].to] == -1 && arr[edgePt].capacity > 0)
+            {
+                depth[arr[edgePt].to] = depth[cntPt] + 1;
+                if (arr[edgePt].to == endPt)
+                    return true;
+                q.push(arr[edgePt].to);
+            }
+            edgePt = arr[edgePt].next;
+        }
+    }
+
+    return false;
+}
+
+int findAguPath(int cntPt, int endPt, int minCapacity)
+{
+    if (cntPt == endPt)
+        return minCapacity;
+    int cntFlow = 0;
+    int &edgePt = lastVisitedEdge[cntPt];
+    while (edgePt != -1)
+    {
+        if (depth[arr[edgePt].to] == depth[cntPt] + 1 && arr[edgePt].capacity > 0)
+        {
+            int flowInc = findAguPath(arr[edgePt].to, endPt, min(minCapacity - cntFlow, arr[edgePt].capacity));
+            maxCapacity = max(maxCapacity, flowInc);
+            if (flowInc == 0)
+            {
+                depth[arr[edgePt].to] = -1;
+            }
+            else
+            {
+                arr[edgePt].capacity -= flowInc;
+                arr[edgePt ^ 1].capacity += flowInc;
+                cntFlow += flowInc;
+
+                if (cntFlow == minCapacity)
+                    break;
+            }
+        }
+        edgePt = arr[edgePt].next;
+    }
+
+    return cntFlow;
+}
+
+int dinic(int startPt, int endPt)
+{
+    int ans = 0;
+    while (updateDepth(startPt, endPt))
+    {
+        for (int i = 0; i < vertexNum; i++)
+            lastVisitedEdge[i] = head[i];
+        int flowInc = findAguPath(startPt, endPt, INT_MAX);
+        if (flowInc == 0)
+            break;
+        ans += flowInc;
+    }
+    return ans;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int caseNum;
+    cin >> caseNum;
+    while (caseNum--)
+    {
+        arrPt = 0;
+        maxCapacity = -1;
+        memset(head, -1, sizeof(head));
+        int caseId, edgeNum, startPt, endPt;
+        cin >> caseId >> vertexNum >> edgeNum >> startPt >> endPt;
+        for (int i = 0; i < edgeNum; i++)
+        {
+            int from, to, length;
+            cin >> from >> to >> length;
+            addEdge(from, to, length);
+        }
+
+        int maxFlow = dinic(startPt, endPt);
+        cout << caseId << " ";
+        cout << fixed << setprecision(3) << (double)maxFlow / maxCapacity << endl;
+    }
+    return 0;
+}
