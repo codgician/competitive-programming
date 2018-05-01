b325e51c15b4fa82c4a622fe61b7b968e9e1d657
Jimmy Zhang <codgician@users.noreply.github.com>
2018-05-01 23:09:10 +0800

    Tabs are evil

--- ZOJ/4029/prefix_sum_binary_search.cpp
+++ ZOJ/4029/prefix_sum_binary_search.cpp
@@ -24,7 +24,7 @@ long long int sumArr[32][SIZE];
 int main()
 {
     int caseNum;
-   	scanf("%d", &caseNum);
+    scanf("%d", &caseNum);
     while (caseNum--)
     {
         int intNum, qNum;
@@ -36,7 +36,7 @@ int main()
         sort(arr + 1, arr + intNum + 1);
         for (int i = 1; i <= 31; i++)
         {
-        	sumArr[i][0] = 0;
+            sumArr[i][0] = 0;
             for (int j = 1; j <= intNum; j++)
             {
                 sumArr[i][j] = (sumArr[i][j - 1] + arr[j] / i) % mod;
@@ -44,23 +44,23 @@ int main()
         }
 
         long long int ans = 0;
-       	for (int i = 1; i <= qNum; i++)
+        for (int i = 1; i <= qNum; i++)
         {
-        	long long int cntAns = 0;
-        	int p;
-        	scanf("%d", &p);
-        	for (int k = 1, leftPt = 1, cnt = 1; k <= 31 && leftPt <= intNum; k++)
-        	{
-        		if (cnt > 1e9 / p)
-        			cnt = 1e9 + 1;
-        		else
-        			cnt *= p;
+            long long int cntAns = 0;
+            int p;
+            scanf("%d", &p);
+            for (int k = 1, leftPt = 1, cnt = 1; k <= 31 && leftPt <= intNum; k++)
+            {
+                if (cnt > 1e9 / p)
+                    cnt = 1e9 + 1;
+                else
+                    cnt *= p;
 
-        		int rightPt = upper_bound(arr + 1, arr + intNum + 1, cnt) - arr;
-        		cntAns = (cntAns + sumArr[k][rightPt - 1] - sumArr[k][leftPt - 1]) % mod;
-        		leftPt = rightPt;
-        	}
-        	ans = (ans + i * cntAns) % mod;
+                int rightPt = upper_bound(arr + 1, arr + intNum + 1, cnt) - arr;
+                cntAns = (cntAns + sumArr[k][rightPt - 1] - sumArr[k][leftPt - 1]) % mod;
+                leftPt = rightPt;
+            }
+            ans = (ans + i * cntAns) % mod;
         }
         ans = (ans + mod) % mod;
         printf("%lld\n", ans);
