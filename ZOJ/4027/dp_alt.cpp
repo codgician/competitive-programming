a9ceac32aaad7651f9072b74f7332e5aefa90716
Jimmy Zhang <codgician@users.noreply.github.com>
2018-05-03 22:56:06 +0800

    ZOJ - 4027: DP (Better)

---
+++ ZOJ/4027/dp_alt.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <climits>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <functional>
+#include <iterator>
+#define LEFT 0
+#define RIGHT 1
+#define SIZE 1010
+using namespace std;
+
+typedef struct _Node
+{
+    bool typeId;
+    int val;
+} Node;
+
+Node arr[SIZE];
+int leftId[SIZE], leftRightId[SIZE];
+long long int rightSum[SIZE];
+long long int dp[SIZE][SIZE];
+
+int main()
+{
+    int caseNum;
+    cin >> caseNum;
+    while (caseNum--)
+    {
+        int len;
+        cin >> len;
+
+        int leftNum = 0, rightNum = 0;
+        for (int i = 1; i <= len; i++)
+        {
+            char ch;
+            cin >> ch;
+            if (ch == '(')
+            {
+                arr[i].typeId = LEFT;
+                leftNum++;
+                leftId[leftNum] = i;
+                leftRightId[leftNum] = rightNum;
+            }
+            else
+            {
+                arr[i].typeId = RIGHT;
+                rightNum++;
+            }
+        }
+
+        rightSum[0] = 0;
+        rightNum = 0;
+        for (int i = 1; i <= len; i++)
+        {
+            cin >> arr[i].val;
+            if (arr[i].typeId == RIGHT)
+            {
+                rightNum++;
+                rightSum[rightNum] = rightSum[rightNum - 1] + arr[i].val;
+            }
+        }
+
+        memset(dp, 0, sizeof(dp));
+        long long int ans = 0;
+        for (int i = leftNum; i >= 1; i--)
+        {
+            for (int j = rightNum; j >= leftRightId[i]; j--)
+            {
+                if (j == rightNum)
+                {
+                    dp[i][j] = dp[i + 1][j] + arr[leftId[i]].val * (rightSum[j] - rightSum[leftRightId[i]]);
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i + 1][j] + arr[leftId[i]].val * (rightSum[j] - rightSum[leftRightId[i]]), dp[i][j + 1]);
+                }
+                ans = max(ans, dp[i][j]);
+            }
+
+            for (int j = leftRightId[i] - 1; j >= leftRightId[i - 1]; j--)
+            {
+                dp[i][j] = dp[i][j + 1];
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
