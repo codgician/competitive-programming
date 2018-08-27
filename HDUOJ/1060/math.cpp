c9d50ee964c3bd6e12d36e115deceb66fc4d6139
Jimmy Zhang <codgician@users.noreply.github.com>
2018-08-27 11:52:31 +0800

    HDUOJ - 1060: Math

---
+++ HDUOJ/1060/math.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+#include <functional>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int caseNum;
+    cin >> caseNum;
+    while (caseNum--)
+    {
+        double n;
+        cin >> n;
+
+        double ans = floor(pow(10, (n * log10(n) - floor(n * log10(n)))));
+        cout << fixed << setprecision(0) << ans << endl;
+    }
+
+    return 0;
+}
