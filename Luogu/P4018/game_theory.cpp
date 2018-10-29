14cd44d0ec899de9b7f753e57e7a85d3ceee670b
Jimmy Zhang <codgician@users.noreply.github.com>
2018-10-29 21:49:20 +0800

    Luogu - P4018: Game theory

---
+++ Luogu/P4018/game_theory.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+#include <functional>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int caseNum;
+    cin >> caseNum;
+    while (caseNum--)
+    {
+        int num;
+        cin >> num;
+
+        if (num % 6 == 0)
+            cout << "Roy wins!" << endl;
+        else
+            cout << "October wins!" << endl;
+    }
+
+    return 0;
+}
