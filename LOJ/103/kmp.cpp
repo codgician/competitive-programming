966fa6cee0c1f63c4234a39ef7289df141988372
Jimmy Zhang <codgician@users.noreply.github.com>
2019-05-26 14:29:13 +0800

    LOJ 103: Update KMP Template

--- LOJ/103/kmp.cpp
+++ LOJ/103/kmp.cpp
@@ -1,55 +1,41 @@
-#include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <cstring>
-#include <iomanip>
-#include <climits>
-#include <vector>
-#include <queue>
-#include <set>
-#include <map>
+#include <bits/stdc++.h>
 #define SIZE 1000100
 using namespace std;
 
 int nextArr[SIZE];
 
-void initNext(string &str)
-{
-    nextArr[0] = 0;
-    for (int i = 1, matchNum = 0; i < (int)str.length(); i++)
-    {
-        while (matchNum > 0 && str[i] != str[matchNum]) // (matchNum + 1) - 1
-            matchNum = nextArr[matchNum - 1];
-        if (str[i] == str[matchNum])
-            matchNum++;
-        nextArr[i] = matchNum;
+void initNext(const string & str) {
+    nextArr[0] = 0; int len = str.size();
+    for (int fstPt = 1, sndPt = 0; fstPt < len; fstPt++) {
+        while (sndPt > 0 && str[fstPt] != str[sndPt])
+            sndPt = nextArr[sndPt - 1];
+        if (str[fstPt] == str[sndPt])
+            sndPt++;
+        nextArr[fstPt] = sndPt;
     }
 }
 
-int kmp(string &a, string &b)
-{
-    int ans = 0;
-    for (int i = 0, matchNum = 0; i < (int)a.length(); i++)
-    {
-        while (matchNum > 0 && (matchNum== (int)b.length() || a[i] != b[matchNum]))
-            matchNum = nextArr[matchNum - 1];
-        if (a[i] == b[matchNum])
-            matchNum++;
-        if (matchNum == (int)b.length())
-            ans++;
+int kmp(const string & fst, const string & snd) {
+    int fstLen = fst.size(), sndLen = snd.size(), ret = 0;
+    for (int fstPt = 0, sndPt = 0; fstPt < fstLen; fstPt++) {
+        while (sndPt > 0 && (sndPt == sndLen || fst[fstPt] != snd[sndPt]))
+            sndPt = nextArr[sndPt - 1];
+        if (fst[fstPt] == snd[sndPt])
+            sndPt++;
+        if (sndPt == sndLen)
+            ret++;
     }
-    return ans;
+    return ret;
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
-    string a, b;
-    cin >> a >> b;
-    initNext(b);
-    int ans = kmp(a, b);
-    cout << ans << endl;
+    cin.tie(0); cout.tie(0);
+
+    string fst, snd;
+    cin >> fst >> snd;
+    initNext(snd);
+
+    cout << kmp(fst, snd) << '\n';
     return 0;
 }
