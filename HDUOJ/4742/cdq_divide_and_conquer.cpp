5ea7462c9ee303151ced67aebf71a1f48b7c3fb6
Shijia Zhang <codgician@users.noreply.github.com>
2019-07-09 23:38:44 +0800

    HDUOJ 4742: Update code style

--- HDUOJ/4742/cdq_divide_and_conquer.cpp
+++ HDUOJ/4742/cdq_divide_and_conquer.cpp
@@ -1,175 +1,116 @@
-#include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <cstring>
-#include <iomanip>
-#include <climits>
-#include <stack>
-#include <queue>
-#include <vector>
-#include <set>
-#include <map>
-#include <functional>
-#include <iterator>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define SIZE 100010
 
 const int mod = 1 << 30;
 
-typedef struct _Node
-{
+typedef struct _Node {
     int x, y, z;
 } Node;
 
-Node arr[SIZE];
+Node arr[SIZE], tmp[SIZE];
 
-typedef struct _Ans
-{
-    int len, num;
+pair<int, int> dp[SIZE], bit[SIZE];
+int dsc[SIZE], len;
 
-    void update(const struct _Ans & snd)
-    {
-        if (len < snd.len)
-        {
-            len = snd.len;
-            num = snd.num;
-        }
-        else if (len == snd.len)
-        {
-            num += snd.num;
-            if (num >= mod)
-                num %= mod;
-        }
-    }
-} Ans;
+void upd(pair<int, int> & ret, const pair<int, int> & val) {
+    if (ret.first < val.first)
+        ret = val;
+    else if (ret.first == val.first)
+        ret.second += val.second, ret.second -= (ret.second >= mod ? mod : 0);
+}
 
-Ans dp[SIZE];
-int num;
-
-int zArr[SIZE];
-Ans bitArr[SIZE];
-
-int getLowbit(int n)
-{
+int lowbit(int n) {
     return n & -n;
 }
 
-void clear(int pos)
-{
-    for (int i = pos; i < SIZE; i += getLowbit(i))
-    {
-        bitArr[i] = {0, 0};
-    }
+void addMax(int pos, const pair<int, int> & val) {
+    pos++;
+    for (int i = pos; i <= len; i += lowbit(i))
+        upd(bit[i], val);
 }
 
-void update(int pos, const Ans & val)
-{
-    for (int i = pos; i < SIZE; i += getLowbit(i))
-    {
-        bitArr[i].update(val);
-    }
+void clear(int pos) {
+    pos++;
+    for (int i = pos; i <= len; i += lowbit(i))
+        bit[i] = make_pair(0, 0);
 }
 
-Ans getPrefixMax(int pos)
-{
-    Ans ans = {0, 0};
-    for (int i = pos; i > 0; i -= getLowbit(i))
-    {
-        ans.update(bitArr[i]);
-    }
-    return ans;
+pair<int, int> prefixMax(int pos) {
+    pair<int, int> ret = make_pair(0, 0); pos++;
+    for (int i = pos; i > 0; i -= lowbit(i))
+        upd(ret, bit[i]);
+    return ret;
 }
 
-bool cmpY(Node & fst, Node & snd)
-{
-    if (fst.y == snd.y)
-        return fst.z < snd.z;
-    return fst.y < snd.y;
+bool cmpSnd(const Node & fst, const Node & snd) {
+    if (fst.y != snd.y)
+        return fst.y < snd.y;
+    return fst.z < snd.z;
 }
 
-bool cmpX(Node & fst, Node & snd)
-{
-    if (fst.x == snd.x)
-        return cmpY(fst, snd);
-    return fst.x < snd.x;
+bool cmpFst(const Node & fst, const Node & snd) {
+    if (fst.x != snd.x)
+        return fst.x < snd.x;
+    return cmpSnd(fst, snd);
 }
 
-void divideConquer(int leftPt, int rightPt)
-{
-    if (leftPt == rightPt)
+void divideConquer(int headPt, int tailPt) {
+    if (headPt == tailPt)
         return;
 
-    int midPt = (leftPt + rightPt) >> 1;
-    divideConquer(leftPt, midPt);
+    int midPt = (headPt + tailPt) >> 1;
+    divideConquer(headPt, midPt);
     
-    sort(arr + leftPt, arr + midPt + 1, cmpY);
-    sort(arr + midPt + 1, arr + rightPt + 1, cmpY);
+    copy(arr + midPt + 1, arr + tailPt + 1, tmp + midPt + 1);
+    sort(arr + midPt + 1, arr + tailPt + 1, cmpSnd);
 
-    int j = leftPt;
-    for (int i = midPt + 1; i <= rightPt; i++)
-    {
+    int j = headPt;
+    for (int i = midPt + 1; i <= tailPt; i++) {
         for (; j <= midPt && arr[j].y <= arr[i].y; j++)
-        {
-            update(arr[j].z + 1, dp[arr[j].x]);
-        }
-
-        Ans cnt = getPrefixMax(arr[i].z + 1);
-        cnt.len++;
-        dp[arr[i].x].update(cnt);
+            addMax(arr[j].z, dp[arr[j].x]);
+        
+        pair<int, int> ret = prefixMax(arr[i].z); ret.first++;
+        upd(dp[arr[i].x], ret);
     }
 
-    for (int i = leftPt; i <= j; i++)
-        clear(arr[i].z + 1);
+    for (int i = headPt; i <= j; i++)
+        clear(arr[i].z);
 
-    sort(arr + midPt + 1, arr + rightPt + 1, cmpX);
-    divideConquer(midPt + 1, rightPt);
+    copy(tmp + midPt + 1, tmp + tailPt + 1, arr + midPt + 1);
+    divideConquer(midPt + 1, tailPt);
+    inplace_merge(arr + headPt, arr + midPt + 1, arr + tailPt + 1, cmpSnd);
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(0); cout.tie(0);
 
-    int caseNum;
-    cin >> caseNum;
-    while (caseNum--)
-    {
-        for (int i = 1; i <= num; i++)
-        {
-            bitArr[i] = {0, 0};
-        }
-
-        cin >> num;
-        for (int i = 0; i < num; i++)
-        {
+    int caseNum; cin >> caseNum;
+    while (caseNum--) {
+        cin >> len;
+        fill(bit + 0, bit + len, make_pair(0, 0));
+        for (int i = 0; i < len; i++) {
             cin >> arr[i].x >> arr[i].y >> arr[i].z;
-            zArr[i] = arr[i].z;
+            dsc[i] = arr[i].z;
         }
 
-        sort(zArr + 0, zArr + num);
-        int zNum = unique(zArr + 0, zArr + num) - zArr;
-        sort(arr + 0, arr + num, cmpX);
+        sort(arr + 0, arr + len, cmpFst);
+        sort(dsc + 0, dsc + len);
+        int dscLen = unique(dsc + 0, dsc + len) - dsc;
 
-        for (int i = 0; i < num; i++)
-        {
-            arr[i].x = i;
-            arr[i].z = lower_bound(zArr + 0, zArr + zNum, arr[i].z) - zArr;
-            dp[i] = {1, 1};
+        for (int i = 0; i < len; i++) {
+            arr[i].z = lower_bound(dsc + 0, dsc + dscLen, arr[i].z) - dsc;
+            arr[i].x = i; dp[i] = make_pair(1, 1);
         }
 
-        divideConquer(0, num - 1);
+        divideConquer(0, len - 1);
 
-        Ans ans = {0, 0};
-        for (int i = 0; i < num; i++)
-        {
-            ans.update(dp[i]);
-        }
-
-        cout << ans.len << " " << ans.num << endl; 
+        pair<int, int> ans = make_pair(0, 0);
+        for (int i = 0; i < len; i++)
+            upd(ans, dp[i]);
+        cout << ans.first << " " << ans.second << endl; 
     }
 
     return 0;
