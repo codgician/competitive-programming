0b09d137c30ca38861f0461512d8bc17f663f78c
Shijia Zhang <codgician@users.noreply.github.com>
2019-12-14 15:04:49 +0800

    Revise CometOJ problem id

--- CometOJ/1174/observation.cpp
+++ CometOJ/1174/observation.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define SIZE 110
+#define SIZE 1010
 
 long long int arr[SIZE];
 
 int init() {
-    arr[1] = 1; arr[2] = 2;
-    int i;
-    for (i = 3; arr[i - 1] <= 1e18; i++)
+    arr[1] = 1, arr[2] = 2; int i;
+    for (i = 3; arr[i - 1] <= 1e18; i++) {
         arr[i] = arr[i - 1] + arr[i - 1] / 2;
+    }
     return i;
 }
 
@@ -17,14 +17,9 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     int arrPt = init();
-
-    long long int num;
-    cin >> num;
+    long long int num; cin >> num;
     int ans = lower_bound(arr + 1, arr + arrPt, num) - arr;
-    if (arr[ans] == num)
-        ans++;
-
+    ans += (arr[ans] == num);
     cout << ans << endl;
-
     return 0;
 }
--- CometOJ/1175/probability_theory.cpp
+++ CometOJ/1175/probability_theory.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long double l, r, L, R, Eq, ans;
+long double l, r, L, R;
 
 long double calc(long double p) {
+    long double Eq = (L + R) / 2;
     return (p - l) * (Eq - p) / (r - l);
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    ans = 0;
     cin >> l >> r >> L >> R;
 
-    Eq = (L + R) / 2;
-    if ((Eq + l) / 2 >= l && (Eq + l) / 2 <= r)
+    long double Eq = (L + R) / 2, ans = 0;
+
+    if ((Eq + l) / 2 <= r && (Eq + l) / 2 >= l)
         ans = max(ans, calc((Eq + l) / 2));
     ans = max(ans, calc(r));
+    ans = max((long double)0, ans);
     cout << fixed << setprecision(4) << ans << endl;
 
     return 0;
 }
-
-
